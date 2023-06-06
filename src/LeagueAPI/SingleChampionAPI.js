3979b7f94507c30eb415e184e43e7a1876a98876
Mateusz <m.wesolowski.92@gmail.com>
2023-06-06 21:35:06 +0200

    update fetching data

--- src/LeagueAPI/SingleChampionAPI.js
+++ src/LeagueAPI/SingleChampionAPI.js
@@ -1,64 +1,43 @@
 import React, { useEffect, useState } from "react";
 import { Spiner } from "../Spiner/Spiner";
+import { ChampionCards } from "../ChampionCards/ChampionCards";
 
 export const SingleChampionAPI = (props) => {
-	const [data, setData] = useState(null);
+	const [data, setData] = useState([]);
 	const [error, setError] = useState(null);
-
+  
 	useEffect(() => {
-		[...props.names].forEach((name) => {
-			(async () => {
-				try {
-					setError(null);
-					const res = await fetch(
-						`http://ddragon.leagueoflegends.com/cdn/13.9.1/data/pl_PL/champion/${name}.json`
-					);
-					const resData = await res.json();
-
-					const infoData = Object.values(resData.data)[0];
-					//const gfdsfgdf = i tu zapisze kolejne dane jak wyżej itd.
-					//const gfdsfgdf = i tu zapisze kolejne dane jak wyżej itd.
-					//const gfdsfgdf = i tu zapisze kolejne dane jak wyżej itd.
-					console.log(Object.values(resData.data)[0]);
-					setData({
-						champName: infoData.name,
-						title: infoData.title,
-						lore: infoData.lore,
-						stats: infoData.stats,
-						passiveSpell: infoData.passive,
-						spells: infoData.spells,
-						tags: infoData.tags,
-						difficulty: infoData.info.difficulty,
-						skinsArr:infoData.skins, //mapujemy
-					});
-				} catch (e) {
-					setError(e);
-				}
-			})();
-		});
+	  const fetchData = async () => {
+		try {
+		  setError(null);
+		  const championData = [];
+		  // Replaced the forEach loop with a for of loop to use await inside the loop.
+		  for (const championName of props.championNames) {
+			const res = await fetch(
+			  `http://ddragon.leagueoflegends.com/cdn/13.9.1/data/pl_PL/champion/${championName}.json`
+			);
+			const resData = await res.json();
+  
+			const infoData = Object.values(resData.data)[0];
+			championData.push(infoData);
+		  }
+		  setData(championData);
+		} catch (e) {
+		  setError(e);
+		}
+	  };
+  
+	  fetchData();
 	}, []);
+  
 
 	if (error) {
-		return <p>{error}</p>;
+	  return <p>{error}</p>;
 	}
-
-	if (data === null) {
-		return <Spiner klasa="Loader"/>;
+  
+	if (data.length === 0) {
+	  return <Spiner className='Loader' />;
 	}
-
-	// console.log(data.spells);
-	return (
-		<>
-
-
-
-			{/* <video
-				src='https://www.leagueoflegends.com/static/what-is-league-of-legends-4e3d9714805dc82373c3a069ed9ec103.mp4'
-				style={{ width: "min", height: "50vh" }}
-				muted
-				loop
-				playsInline
-				autoPlay></video> */}
-		</>
-	);
-};
+  
+	return <ChampionCards championsData={data}/>;
+  };

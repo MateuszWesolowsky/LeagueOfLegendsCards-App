202e1afcb92c9cc022a234d2a3ddff16431eb466
Mateusz <m.wesolowski.92@gmail.com>
2023-06-06 21:36:35 +0200

    add components to app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from "react";
 import "./App.css";
 import { AllChampionsAPI } from "./LeagueAPI/AllChampionsAPI";
-
+import { Header } from "./Header/Header";
+import { Navigation } from "./Navigation/Navigation";
+import { ChampionCards } from "./ChampionCards/ChampionCards";
 
 export const App = () => {
 	return (
 		<div className='App'>
+			<Header />
+			<Navigation />
+			<ChampionCards />
 			<AllChampionsAPI/>
 		</div>
 	);

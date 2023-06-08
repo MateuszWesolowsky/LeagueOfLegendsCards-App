af8ffc8e3299f921984b0db65f40e9cdbc6031bd
Mateusz <m.wesolowski.92@gmail.com>
2023-06-08 23:57:16 +0200

    create championCard comp

--- src/ChampionCards/ChampionCards.js
+++ src/ChampionCards/ChampionCards.js
@@ -1,23 +1,31 @@
 import React from "react";
-
+import "./ChampionCards.css";
+import { Link, Route, Routes } from "react-router-dom";
+import { ChampionView } from "../ChampionView/ChampionView";
 
 export const ChampionCards = ({ championsData }) => {
-	if (!championsData) {
-		return ;
-	}
-
-
 
 	return (
 		<div className='Champion__cards__container'>
-			<ul>
+			<ul className='cards'>
 				{championsData.map((card) => {
-					return <li key={card.id}>{card.name}</li>;
+					return (
+						<li key={card.id} className='card'>
+							<div className='cardana'>
+								<Link
+									to={`/${card.id.toLowerCase()}`}>
+									<img
+										src={`http://ddragon.leagueoflegends.com/cdn/img/champion/loading/${card.id}_0.jpg`}
+										alt={`Champion picture - ${card.name}`}
+									/>
+								</Link>
+
+								{card.name}
+							</div>
+						</li>
+					);
 				})}
 			</ul>
 		</div>
 	);
 };
-
-
-

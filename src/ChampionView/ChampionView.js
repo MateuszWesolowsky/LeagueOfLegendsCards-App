2b7d62c2d1e191779e3776b670e8345075e6114b
Mateusz <m.wesolowski.92@gmail.com>
2023-06-12 23:55:54 +0200

    create ChampionView component

--- src/ChampionView/ChampionView.js
+++ src/ChampionView/ChampionView.js
@@ -4,7 +4,9 @@ import "./ChampionView.css";
 export const ChampionView = ({singleChampView}) => {
 	return (
 		<>
-			<div className='test'></div>
+			<div className='test'>
+			coś w diviecoś w diviecoś w diviecoś w diviecoś w diviecoś w diviecoś w diviecoś w diviecoś w diviecoś w diviecoś w diviecoś w diviecoś w diviecoś w diviecoś w diviecoś w diviecoś w diviecoś w diviecoś w diviecoś w diviecoś w diviecoś w diviecoś w diviecoś w diviecoś w diviecoś w diviecoś w diviecoś w diviecoś w diviecoś w diviecoś w diviecoś w diviecoś w diviecoś w diviecoś w diviecoś w diviecoś w diviecoś w diviecoś w diviecoś w diviecoś w diviecoś w diviecoś w divie
+			</div>
 		</>
 	);
 };

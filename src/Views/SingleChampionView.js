80d7d7384fe56887cc92687ddfe00408b5f18cb8
Mateusz <m.wesolowski.92@gmail.com>
2023-06-13 23:34:27 +0200

    create function with API from params for each champ

---
+++ src/Views/SingleChampionView.js
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { Spinner } from "../Spinner/Spinner";
+import { NotFoundView } from "./NotFoundView";
+import { OneChampionCard } from "../ChampionCards/OneChampionCard";
+
+export const SingleChampionView = () => {
+	const [championInfo, setChampionInfo] = useState(null);
+	const [error, setError] = useState(null);
+	const { championId } = useParams();
+
+	useEffect(() => {
+		(async () => {
+			try {
+				setError(null);
+				const res = await fetch(
+					`http://ddragon.leagueoflegends.com/cdn/13.9.1/data/pl_PL/champion/${championId}.json`
+				);
+				const resData = await res.json();
+				setChampionInfo(Object.values(resData.data)[0]);
+			} catch (e) {
+				setError(e);
+			}
+		})();
+	}, [championId]);
+
+	console.log(championInfo);
+	if (error) {
+		return <NotFoundView />;
+	}
+
+	if (championInfo === null) {
+		return <Spinner />;
+	}
+
+	return <OneChampionCard singleChampionInfo={championInfo} />;
+};

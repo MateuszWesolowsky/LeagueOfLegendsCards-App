9e493c16321902e8429c33a2fe787508fc07b1df
Mateusz <m.wesolowski.92@gmail.com>
2023-06-13 23:32:44 +0200

    add more components to MainView file

--- src/Views/MainView.js
+++ src/Views/MainView.js
@@ -3,7 +3,7 @@ import { AllChampionsAPI } from "../LeagueAPI/AllChampionsAPI";
 import { Navigation } from "../Navigation/Navigation";
 import { Header } from "../Header/Header";
 
-//dodac drugi komponent czyli nav i wtedy jeden renderujemy glowny
+
 
 export const MainView = () => {
 	return (

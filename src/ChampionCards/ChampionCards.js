27a1cd149f63d26b27121ffa2f2b9b058ae94563
Mateusz <m.wesolowski.92@gmail.com>
2023-06-16 23:27:57 +0200

    add styles and more info about champion

--- src/ChampionCards/ChampionCards.js
+++ src/ChampionCards/ChampionCards.js
@@ -5,18 +5,21 @@ import { Link } from "react-router-dom";
 export const ChampionCards = ({ championsData }) => {
 	return (
 		<div className='Champion__cards__container'>
-			<ul className='cards'>
+			<ul className='Cards'>
 				{championsData.map((card) => {
 					return (
-						<li key={card.id} className='card'>
+						<li key={card.id} className='One__card'>
 							<div className='Champ__img__container'>
-								<Link to={`/${card.id}`}>
-									<img
-										src={`http://ddragon.leagueoflegends.com/cdn/img/champion/loading/${card.id}_0.jpg`}
-										alt={`Champion picture - ${card.name}`}
-									/>
-								</Link>
-								{card.name}
+								<div className='OneChamp__card'>
+									<Link to={`/${card.id}`}>
+										<img
+											src={`http://ddragon.leagueoflegends.com/cdn/img/champion/loading/${card.id}_0.jpg`}
+											alt={`Champion picture - ${card.name}`}
+										/>
+									
+									<span>{card.name}</span>
+									</Link>
+								</div>
 							</div>
 						</li>
 					);

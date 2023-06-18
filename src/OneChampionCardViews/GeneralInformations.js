3504d72b311d17628617a03af4061b3590a058ca
Mateusz <m.wesolowski.92@gmail.com>
2023-06-18 23:32:35 +0200

    create comp for general info in single champ view

---
+++ src/OneChampionCardViews/GeneralInformations.js
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+import "./GeneralInformations.css";
+
+export const GeneralInformations = ({ singleChampionInfo }) => {
+	const [visibleText, setVisibleText] = useState(true);
+
+	const checkMore = () => {
+		setVisibleText(false);
+	};
+
+	return (
+		<div className='General__informations'>
+			<div className='Img__container'>
+				<img
+					className='Blur__img'
+					src={`http://ddragon.leagueoflegends.com/cdn/img/champion/splash/${singleChampionInfo.id}_0.jpg`}
+					alt={`Champion - ${singleChampionInfo.id}`}
+				/>
+				<img
+					className='Main__img'
+					src={`http://ddragon.leagueoflegends.com/cdn/img/champion/splash/${singleChampionInfo.id}_0.jpg`}
+					alt={`Champion - ${singleChampionInfo.id}`}
+				/>
+				<div className='Champion__title__container'>
+					<span>{singleChampionInfo.title}</span>
+					<h2>{singleChampionInfo.name}</h2>
+				</div>
+			</div>
+			<div className='Champion__description__container'>
+				<div className='Left__info__container'>
+					<div className='Role__container'>
+						<span>Rola</span>
+						<span>{singleChampionInfo.tags[0]}</span>
+					</div>
+					<div className='Difficulty__level__container'>
+						<span>Poziom trudności</span>
+						<span>{singleChampionInfo.info.difficulty}</span>
+					</div>
+				</div>
+				<div className='Right__info__container'>
+					{visibleText && (
+						<p>
+							{singleChampionInfo.blurb}
+							<button onClick={checkMore}>zobacz więcej</button>
+						</p>
+					)}
+					{!visibleText && <p>{singleChampionInfo.lore}</p>}
+				</div>
+			</div>
+		</div>
+	);
+};

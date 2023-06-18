6a9223c8543ecd0d9b6ed1cff82a262ae36d12eb
Mateusz <m.wesolowski.92@gmail.com>
2023-06-18 23:32:06 +0200

    spreaded data to single components

--- src/ChampionCards/OneChampionCard.js
+++ src/ChampionCards/OneChampionCard.js
@@ -1,54 +1,15 @@
-import React, { useState } from "react";
+import React from "react";
 
 import "./OneChampionCard.css";
+import { GeneralInformations } from "../OneChampionCardViews/GeneralInformations";
+import { Spells } from "../OneChampionCardViews/Spells";
 
 export const OneChampionCard = ({ singleChampionInfo }) => {
-	const [visibleText, setVisibleText] = useState(true);
-
-	const checkMore = () => {
-		setVisibleText(false);
-	};
-
 	return (
 		<>
 			<div className='Champion__container'>
-				<div className='Img__container'>
-					<img
-						className='Blur__img'
-						src={`http://ddragon.leagueoflegends.com/cdn/img/champion/splash/${singleChampionInfo.id}_0.jpg`}
-						alt={`Champion - ${singleChampionInfo.id}`}
-					/>
-					<img
-						className='Main__img'
-						src={`http://ddragon.leagueoflegends.com/cdn/img/champion/splash/${singleChampionInfo.id}_0.jpg`}
-						alt={`Champion - ${singleChampionInfo.id}`}
-					/>
-					<div className='Champion__title__container'>
-						<span>{singleChampionInfo.title}</span>
-						<h2>{singleChampionInfo.name}</h2>
-					</div>
-				</div>
-				<div className='Champion__description__container'>
-					<div className='Left__info__container'>
-						<div className='Role__container'>
-							<span>Rola</span>
-							<span>{singleChampionInfo.tags[0]}</span>
-						</div>
-						<div className='Difficulty__level__container'>
-							<span>Poziom trudności</span>
-							<span>{singleChampionInfo.info.difficulty}</span>
-						</div>
-					</div>
-					<div className='Right__info__container'>
-						{visibleText && (
-							<p>
-								{singleChampionInfo.blurb}
-								<button onClick={checkMore}>zobacz więcej</button>
-							</p>
-						)}
-						{!visibleText && <p>{singleChampionInfo.lore}</p>}
-					</div>
-				</div>
+				<GeneralInformations singleChampionInfo={singleChampionInfo} />
+				<Spells singleChampionInfo={singleChampionInfo} />
 			</div>
 		</>
 	);

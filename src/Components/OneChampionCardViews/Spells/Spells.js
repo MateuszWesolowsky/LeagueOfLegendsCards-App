df3b611ab1378f0539993036493fec97fbf48fbf
Mateusz <m.wesolowski.92@gmail.com>
2023-09-05 21:44:01 +0200

    rwd styles and change classess names

--- src/Components/OneChampionCardViews/Spells/Spells.js
+++ src/Components/OneChampionCardViews/Spells/Spells.js
@@ -11,46 +11,51 @@ export const Spells = ({ singleChampionInfo }) => {
 	};
 
 	return (
-		<div className='Spells__container'>
-			<h2 className='Spells__h2'>Umiejętności</h2>
-
-			<div className='Spells'>
-				<div className='Spells__img__container'>
+		<div className='spells-container'>
+			<h2>Umiejętności</h2>
+			<div className='spells-img'>
+				<img
+					onClick={() => chosedSpell(singleChampionInfo.passive.name)}
+					className={`${
+						activeSpellName === singleChampionInfo.passive.name
+							? "shadow-img"
+							: ""
+					}`}
+					src={`https://ddragon.leagueoflegends.com/cdn/13.17.1/img/passive/${singleChampionInfo.passive.image.full}`}
+					alt={`Passive spell - ${singleChampionInfo.name}`}
+				/>
+				{singleChampionInfo.spells.map((spell) => (
 					<img
-						onClick={() => chosedSpell(singleChampionInfo.passive.name)}
-						src={`https://ddragon.leagueoflegends.com/cdn/13.17.1/img/passive/${singleChampionInfo.passive.image.full}`}
-						alt={`Passive spell - ${singleChampionInfo.name}`}
+						className={`${
+							activeSpellName === spell.name ? "shadow-img" : ""
+						}`}
+						onClick={() => chosedSpell(spell.name)}
+						key={spell.id}
+						src={`https://ddragon.leagueoflegends.com/cdn/13.17.1/img/spell/${spell.id}.png`}
+						alt={`${activeSpellName} spell - ${singleChampionInfo.name}`}
 					/>
-					{singleChampionInfo.spells.map((spell) => (
-						<img
-							onClick={() => chosedSpell(spell.name)}
-							key={spell.id}
-							src={`https://ddragon.leagueoflegends.com/cdn/13.17.1/img/spell/${spell.id}.png`}
-							alt={`Passive spell - ${singleChampionInfo.name}`}
-						/>
-					))}
-				</div>
-				<div className='Spells__description__container'>
-					<span className='Spells__names'>
-						{activeSpellName === singleChampionInfo.passive.name
-							? singleChampionInfo.passive.name
-							: singleChampionInfo.spells.find(
-									(spellName) => spellName.name === activeSpellName
-							  ).name}
-					</span>
-					<span className='Spells__description'>
-						{activeSpellName === singleChampionInfo.passive.name
-							? singleChampionInfo.passive.description
-									.replaceAll("<", "")
-									.replaceAll(">", "")
-									.replaceAll("/", "")
-							: singleChampionInfo.spells
-									.find((spellName) => spellName.name === activeSpellName)
-									.description.replaceAll("<", "")
-									.replaceAll(">", "")
-									.replaceAll("/", "")}
-					</span>
-				</div>
+				))}
+			</div>
+			<div className='spells-description-container'>
+				<h2 className='spell-name'>
+					{activeSpellName === singleChampionInfo.passive.name
+						? singleChampionInfo.passive.name
+						: singleChampionInfo.spells.find(
+								(spellName) => spellName.name === activeSpellName
+						  ).name}
+				</h2>
+				<span className='spell-description'>
+					{activeSpellName === singleChampionInfo.passive.name
+						? singleChampionInfo.passive.description
+								.replaceAll("<", "")
+								.replaceAll(">", "")
+								.replaceAll("/", "")
+						: singleChampionInfo.spells
+								.find((spellName) => spellName.name === activeSpellName)
+								.description.replaceAll("<", "")
+								.replaceAll(">", "")
+								.replaceAll("/", "")}
+				</span>
 			</div>
 		</div>
 	);

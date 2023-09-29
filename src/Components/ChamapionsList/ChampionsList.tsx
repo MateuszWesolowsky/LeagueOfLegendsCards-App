977d8f1ecd7a145fbc5ae608beb2531e9e549159
Mateusz <m.wesolowski.92@gmail.com>
2023-09-29 22:45:45 +0200

    add types and refactor code

--- src/Components/ChamapionsList/ChampionsList.tsx
+++ src/Components/ChamapionsList/ChampionsList.tsx
@@ -2,55 +2,51 @@ import React from "react";
 import { Spinner } from "../Spinner/Spinner";
 import { Link } from "react-router-dom";
 import "../ChamapionsList/ChamapionsList.css";
-
-interface ChampionInfo {
-  id: string;
-  name: string;
-}
+import ChampionInfo from "../../types/type";
 
 interface ChampionsListProps {
-  championsInfo: ChampionInfo[];
-  error: string | null;
-  isLoading: boolean;
-  query: string;
+	championsInfo: ChampionInfo[];
+	error: string | null;
+	isLoading: boolean;
+	query: string;
 }
 
 export const ChampionsList: React.FC<ChampionsListProps> = ({
-  championsInfo,
-  error,
-  isLoading,
-  query,
+	championsInfo,
+	error,
+	isLoading,
+	query,
 }) => {
-  if (error) {
-    return <p>{error}</p>;
-  }
-  if (isLoading) {
-    return <Spinner className={"loader"} />;
-  }
+	if (error) {
+		return <p>{error}</p>;
+	}
+	if (isLoading) {
+		return <Spinner className={"loader"} />;
+	}
 
-  return (
-    <main className="champions-list-container">
-      <ul className="champions-cards-list">
-        {championsInfo.length === 0 ? (
-          <p className="not-found">Champion {query} not found...</p>
-        ) : (
-          championsInfo.map((card) => {
-            return (
-              <li key={card.id} className="one-card-container">
-                <div className="card-img-container">
-                  <Link to={`/${card.id}`}>
-                    <img
-                      src={`http://ddragon.leagueoflegends.com/cdn/img/champion/loading/${card.id}_0.jpg`}
-                      alt={`Champion - ${card.name}`}
-                    />
-                  </Link>
-                  <span>{card.name}</span>
-                </div>
-              </li>
-            );
-          })
-        )}
-      </ul>
-    </main>
-  );
+	return (
+		<main className='champions-list-container'>
+			<ul className='champions-cards-list'>
+				{championsInfo.length === 0 ? (
+					<p className='not-found'>Champion {query} not found...</p>
+				) : (
+					championsInfo.map((card) => {
+						return (
+							<li key={card.id} className='one-card-container'>
+								<div className='card-img-container'>
+									<Link to={`/${card.id}`}>
+										<img
+											src={`http://ddragon.leagueoflegends.com/cdn/img/champion/loading/${card.id}_0.jpg`}
+											alt={`Champion - ${card.name}`}
+										/>
+									</Link>
+									<span>{card.name}</span>
+								</div>
+							</li>
+						);
+					})
+				)}
+			</ul>
+		</main>
+	);
 };

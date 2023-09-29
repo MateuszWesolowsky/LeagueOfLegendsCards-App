cd2189d82513005ea228698a909fd07e8256e076
Mateusz <m.wesolowski.92@gmail.com>
2023-09-29 22:51:04 +0200

    update file to typescript

--- src/Components/Views/NotFoundView.tsx
+++ src/Components/Views/NotFoundView.tsx
@@ -2,13 +2,16 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./NotFoundView.css";
 
-export const NotFoundView = () => {
+export const NotFoundView: React.FC = () => {
 	return (
 		<div className='NotFound__Container'>
 			<h2>Page not found</h2>
 			<p>
 				We're sorry, the page you requested could not be found. Please go back
-				to the <Link className="Home__link" to={`/`}>HOME PAGE</Link>
+				to the
+				<Link className='Home__link' to={`/`}>
+					HOME PAGE
+				</Link>
 			</p>
 		</div>
 	);

3276a266a8a028c23eb2080a3929af5cf7fb0fd9
Mateusz <m.wesolowski.92@gmail.com>
2023-09-29 22:50:53 +0200

    update file to typescript

--- src/Components/Views/MainView.tsx
+++ src/Components/Views/MainView.tsx
@@ -1,41 +1,45 @@
 import React, { useState } from "react";
-import { ChampionsList } from "../ChamapionsList/ChampionsList.tsx";
-import { Navigation } from "../Navigation/Navigation.tsx";
-import { Header } from "../Header/Header.tsx";
+import { Navigation } from "../Navigation/Navigation";
+import { Header } from "../Header/Header";
+import { ChampionsList } from "../ChamapionsList/ChampionsList";
 import { useFetchChampions } from "../Hooks/useFetchChampions";
+import ChampionInfo from "../../types/type";
 
 const API_URL =
 	"https://ddragon.leagueoflegends.com/cdn/13.17.1/data/pl_PL/champion.json";
-export const MainView = () => {
+
+export const MainView: React.FC = () => {
 	const { championsInfo, error, isLoading } = useFetchChampions(API_URL);
 	const [selectedCategory, setSelectedCategory] = useState("All");
 	const [query, setQuery] = useState("");
-	const [level, setLevel] = useState("Wszystkie poziomy trudości");
+	const [level, setLevel] = useState("Wszystkie poziomy trudności");
 
-	const handleSelectCategory = (category) => {
+	const handleSelectCategory = (category: string) => {
 		setSelectedCategory(category);
 		setQuery("");
 	};
 
-	let filteredChampions;
+	let filteredChampions: ChampionInfo[] = championsInfo;
 
-	filteredChampions = championsInfo.filter((champion) =>
-		selectedCategory === "All"
-			? champion
-			: champion.tags.includes(selectedCategory)
-	);
+	if (selectedCategory !== "All") {
+		filteredChampions = championsInfo.filter((champion) =>
+			champion.tags.includes(selectedCategory)
+		);
+	}
 
-	filteredChampions = filteredChampions.filter((champion) =>
-		champion.id.toLowerCase().includes(query.toLocaleLowerCase())
-	);
+	if (query !== "") {
+		filteredChampions = filteredChampions.filter((champion) =>
+			champion.id.toLowerCase().includes(query.toLowerCase())
+		);
+	}
 
 	filteredChampions = filteredChampions.filter((champion) => {
 		if (level === "Łatwy") {
 			return champion.info.difficulty < 4;
 		} else if (level === "Średni") {
-			return champion.info.difficulty > 4 && champion.info.difficulty < 7;
+			return champion.info.difficulty >= 4 && champion.info.difficulty < 7;
 		} else if (level === "Trudny") {
-			return champion.info.difficulty > 7;
+			return champion.info.difficulty >= 7;
 		}
 		return true;
 	});

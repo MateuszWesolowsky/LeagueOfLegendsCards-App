6b20db48ac258f99a8db78cfff324c2abdd18850
Mateusz <m.wesolowski.92@gmail.com>
2023-09-29 22:51:17 +0200

    update file to typescript

--- src/Components/Views/SingleChampionView.tsx
+++ src/Components/Views/SingleChampionView.tsx
@@ -1,19 +1,20 @@
 import React, { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { Spinner } from "../Spinner/Spinner";
-import { GeneralInformations } from "../OneChampionCardViews/GeneralInformations/GeneralInformations.tsx";
-import { Spells } from "../OneChampionCardViews/Spells/Spells.tsx";
+import { GeneralInformations } from "../OneChampionCardViews/GeneralInformations/GeneralInformations";
+import { Spells } from "../OneChampionCardViews/Spells/Spells";
 import { Skins } from "../OneChampionCardViews/Skins/Skins";
 import { useFetchChampions } from "../Hooks/useFetchChampions";
+import ChampionInfo from "../../types/type";
 
-export const SingleChampionView = () => {
-	const { championId } = useParams();
+export const SingleChampionView: React.FC = () => {
+	const { championId } = useParams<{ championId: string }>();
 
 	const API = `https://ddragon.leagueoflegends.com/cdn/13.17.1/data/pl_PL/champion/${championId}.json`;
 
 	const { championsInfo, error, isLoading } = useFetchChampions(API);
 
-	let championData = championsInfo[0];
+	const championData: ChampionInfo | undefined = championsInfo[0];
 
 	useEffect(() => {
 		document.title = `Champion | ${championId}`;
@@ -25,17 +26,15 @@ export const SingleChampionView = () => {
 	if (error) {
 		return <p>{error}</p>;
 	}
-	if (isLoading) {
+	if (isLoading || !championData) {
 		return <Spinner className='loader' />;
 	}
 
 	return (
-		championData && (
-			<>
-				<GeneralInformations singleChampionInfo={championData} />
-				<Spells singleChampionInfo={championData} />
-				<Skins singleChampionInfo={championData} />
-			</>
-		)
+		<>
+			<GeneralInformations singleChampionInfo={championData} />
+			<Spells singleChampionInfo={championData} />
+			<Skins singleChampionInfo={championData} />
+		</>
 	);
 };

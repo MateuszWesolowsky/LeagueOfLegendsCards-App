c235f17cad8f20cbd20becbd99c10fe17fbcccd1
Mateusz <m.wesolowski.92@gmail.com>
2023-10-01 19:37:37 +0200

    create mock fc and test with arg behavior

--- src/Tests/Header.test.tsx
+++ src/Tests/Header.test.tsx
@@ -1,10 +1,30 @@
 import { render, screen } from "@testing-library/react";
 import { Header } from "../Components/Header/Header";
 import "@testing-library/jest-dom/extend-expect";
+import userEvent from "@testing-library/user-event";
 
-test("renders Header component", () => {
-	render(<Header />);
+describe("Header component", () => {
+	test("renders Header component", () => {
+		render(<Header />);
 
-	const headerElement = screen.getByText("CZYM JEST LEAGUE OF LEGENDS?");
-	expect(headerElement).toBeInTheDocument();
+		const headerElement = screen.getByText("CZYM JEST LEAGUE OF LEGENDS?");
+		expect(headerElement).toBeInTheDocument();
+	});
+
+	test("scrolls to section when button is clicked", () => {
+		render(<Header />);
+
+		const button = screen.getByText("Poznaj bohaterów");
+
+		const section = screen.getByTestId("header-title");
+
+		const scrollIntoViewMock = jest.fn();
+		Element.prototype.scrollIntoView = scrollIntoViewMock;
+
+		userEvent.click(button);
+
+		expect(section.scrollIntoView).toHaveBeenCalledWith({
+			behavior: "smooth",
+		});
+	});
 });
